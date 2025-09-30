c800aeaf358d0b693f00ce708d49640b7555cdd4
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-09-30 13:23:23 +0330

    added placeholder for home page and 404 page for now

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+// Codes by mahdi tasha
+// Importing part
+import { JSX } from "react";
+
+// Creating and exporting NotFound page as default
+export default function NotFoundPage(): JSX.Element {
+  // Returning JSX
+  return (
+    <div className="h-dvh overflow-hidden flex items-center justify-center">
+      <h1>The page you are looking for is not found !</h1>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// Codes by mahdi tasha
+// Importing part
+import { JSX } from "react";
+
+// Creating and exporting Home page as default
+export default function HomePage(): JSX.Element {
+  // Returning JSX
+  return (
+    <div className="h-dvh overflow-hidden flex items-center justify-center">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
