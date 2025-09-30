1b78327d9742fa0808e3c6eb7591f18407bd9b9a
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-09-30 14:04:36 +0330

    added new theme for the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 // Codes by mahdi tasha
 // Importing part
+import { Button } from "@/components/ui/button";
 import { JSX } from "react";
 
 // Creating and exporting Home page as default
 export default function HomePage(): JSX.Element {
   // Returning JSX
   return (
-    <div className="h-dvh overflow-hidden flex items-center justify-center">
-      <h1>Home Page</h1>
+    <div className="h-dvh overflow-hidden flex items-center gap-5 justify-center p-5 flex-wrap">
+      <Button variant={"default"}>CLICK HERE</Button>
+      <Button variant={"destructive"}>CLICK HERE</Button>
+      <Button variant={"link"}>CLICK HERE</Button>
+      <Button variant={"outline"}>CLICK HERE</Button>
+      <Button variant={"secondary"}>CLICK HERE</Button>
+      <Button variant={"ghost"}>CLICK HERE</Button>
     </div>
   );
 }
